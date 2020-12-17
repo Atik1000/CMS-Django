4d07c95d9c05e6e8a49c675009bf7bd9d563f3c4
Atik1000 <foysalkazi45@gmail.com>
2020-12-17 21:33:35 +0600

    problem face

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 
 def home(request):
-    return render(request, 'accounts/dashbord.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    total_customers = customers.count()
+    total_ordered = orders.count()
+
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+
+    context = {'orders': orders, 'customers': customers, 'total_ordered': total_ordered, 'delivered': delivered,
+               'pending': pending}
+    return render(request, 'accounts/dashbord.html', context)
 
 
 def products(request):
-    return render(request, 'accounts/product.html')
+    product = Product.objects.all()
+    context = {'pd': product}
+    return render(request, 'accounts/product.html', context)
 
 
 def customer(request):
