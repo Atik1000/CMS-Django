218ff745ddc36d0549421f488d5f4b669c849588
Atik1000 <foysalkazi45@gmail.com>
2020-12-22 23:57:36 +0600

    issu added

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,12 +2,15 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', home, name='home'),
     path('login/', login_page, name='login'),
     path('logout/', logout_page, name='logout'),
-    path('register', reg_page, name='register'),
+    path('register/', reg_page, name='register'),
+
+    path('', home, name='home'),
+    path('user/', user_page, name="user-page"),
     path('products/', products, name='products'),
     path('customer/<str:pk>/', customer, name='customer'),
+
     path('create_order/', createOrder, name='create-order'),
     path('update_order/<str:pk>/', updateOrder, name='update-order'),
     path('delete/<str:pk>', deleteOrder, name='delete'),
--- accounts/views.py
+++ accounts/views.py
@@ -2,45 +2,51 @@ from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
 from .models import *
 from django.contrib.auth.forms import UserCreationForm
+from .decorators import *
 from .forms import *
 from .filters import OrderFilter
 from .forms import SignUpForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth import authenticate, login, logout
 
 
+@unauthernticated_user
 def reg_page(request):
-    if request.user.is_authenticated:
-        return redirect('home')
     form = SignUpForm()
-    register=False
+    register = False
     if request.method == "POST":
         form = SignUpForm(data=request.POST)
         if form.is_valid():
             form.save()
-            register=True
-            user=form.cleaned_data.get('username')
-            messages.success(request,'Account created successfully for' + user)
+            register = True
+            user = form.cleaned_data.get('username')
+            customer_qs = Customer.objects.create(
+                name=form.cleaned_data.get('username'), email=form.cleaned_data.get('email'),
+                phone=form.cleaned_data.get('phone', 12345))
+            messages.success(
+                request, 'Account created successfully for' + user)
             return redirect('login')
-    context = {'form': form,'register':register}
+    context = {'form': form, 'register': register}
     return render(request, 'accounts/registration.html', context)
 
 
+@unauthernticated_user
 def login_page(request):
     if request.method == 'POST':
-       username= request.POST.get('username')
-       password= request.POST.get('password')
-       user = authenticate(request ,username=username,password=password)
-       
-       if user is not None: 
-           login(request,user)
-           return redirect('home')
-       else:
-           messages.info(request,"username OR password is incorrect")
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, "username OR password is incorrect")
     context = {}
 
     return render(request, 'accounts/login.html', context)
+
 
 def logout_page(request):
     logout(request)
@@ -48,6 +54,8 @@ def logout_page(request):
 
 
 @login_required(login_url='login')
+# @allowed_users(allowed_roles=
+@admin_only
 def home(request):
     orders = Order.objects.all()
     customers = Customer.objects.all()
@@ -62,11 +70,18 @@ def home(request):
                'pending': pending}
     return render(request, 'accounts/dashbord.html', context)
 
+
+def user_page(request):
+    context = {}
+    return render(request, 'accounts/user.html', context)
+
+
 @login_required(login_url='login')
 def products(request):
     product = Product.objects.all()
     context = {'pd': product}
     return render(request, 'accounts/product.html', context)
+
 
 @login_required(login_url='login')
 def customer(request, pk):
@@ -75,8 +90,10 @@ def customer(request, pk):
     orders_count = orders.count()
     my_filter = OrderFilter(request.GET, queryset=orders)
     orders = my_filter.qs
-    context = {'customer': customers, 'order': orders, 'orders_count': orders_count, 'myFilter': my_filter}
+    context = {'customer': customers, 'order': orders,
+               'orders_count': orders_count, 'myFilter': my_filter}
     return render(request, 'accounts/customer.html', context)
+
 
 @login_required(login_url='login')
 def createOrder(request):
@@ -89,7 +106,9 @@ def createOrder(request):
     context = {'form': forms}
     return render(request, 'accounts/order_form.html', context)
 
+
 @login_required(login_url='login')
+@allowed_users(allowed_roles=['admin'])
 def updateOrder(request, pk):
     order = Order.objects.get(id=pk)
     form = OrderForm(instance=order)
@@ -103,8 +122,11 @@ def updateOrder(request, pk):
 
     return render(request, 'accounts/order_form.html', context)
 
+
 @login_required(login_url='login')
+@allowed_users(allowed_roles=['admin'])
 def deleteOrder(request, pk):
+
     order = Order.objects.get(id=pk)
     order.delete()
     # order.save()
